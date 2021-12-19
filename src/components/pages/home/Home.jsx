88c8d8ed24352e8a59baaf484f8b3c9c3845769d
huzaifa <huzaifa98nur@gmail.com>
2021-12-20 03:36:22 +0500

    bugs fixed

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,9 @@
-import Chart from "../../components/chart/Chart";
-import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../../components/chart/Chart";
+import FeaturedInfo from "../../featuredInfo/FeaturedInfo";
 import "./home.css";
-import { data } from "../../dummyData";
-import WidgetSm from "../../components/widgetSm/WidgetSm";
-import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { data } from "../../../dummyData";
+import WidgetSm from "../../widgetSm/WidgetSm";
+import WidgetLg from "../../widgetLg/WidgetLg";
 
 export default function Home() {
   return (
--- src/components/pages/userList/UserList.jsx
+++ src/components/pages/userList/UserList.jsx
@@ -2,7 +2,7 @@ import "./userList.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { Link } from "react-router-dom";
-import { userRows } from "../../dummyData";
+import { userRows } from "../../../dummyData";
 import { useState } from "react";
 
 export default function UserList() {
